1be81a28f50df86f585869da59e160518387caaa
Eka Pramudita <ekaapramudita7@gmail.com>
2021-04-16 22:17:48 +0700

    Extract Transform Done

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import luigi
-from src import LoaderFinished
+from src.extract_transform import ExtractCompleted
+
+def main():
+
+    luigi.run(main_task_cls=ExtractCompleted, local_scheduler=False)
+
+
+if __name__ == "__main__":
+    main()
--- src/extract_transform.py
+++ src/extract_transform.py
@@ -108,6 +108,9 @@ class Disaster(luigi.Task):
 
     def run(self):
 
+        disaster = pd.read_csv('./data/disaster_data.csv').to_csv('./src/extracted/disaster.csv',
+                                                                  encoding='utf-8', index=False, header=True)
+
         self.completed = True
 
 
@@ -125,6 +128,37 @@ class Reviews(luigi.Task):
 
     def run(self):
 
+        # Compare 2 identical files and check for duplicates, reviews q1
+        q1_csv = pd.read_csv('./data/reviews_q1.csv')
+        q1_xlsx = pd.read_excel('./data/reviews_q1.xlsx')
+
+        # Adjust date data types
+        q1_csv['date'] = pd.to_datetime(q1_csv['date']).dt.date
+        q1_xlsx['date'] = pd.to_datetime(q1_xlsx['date']).dt.date
+
+        # Use word count instead of comments
+        q1_csv['word_count'] = q1_csv['comments'].str.split().str.len()
+        q1_xlsx['word_count'] = q1_xlsx['comments'].str.split().str.len()
+
+        # Drop Duplicates excludes comments
+        q1_all = pd.concat([q1_csv.drop('comments', 1),q1_xlsx.drop('comments', 1)])
+        q1_all = q1_all.drop_duplicates()
+        q1 = q1_all.merge(q1_csv[['id', 'comments']]).drop('word_count', 1)
+
+        # Put together with another quarter
+        q2 = pd.read_csv('./data/reviews_q2.csv')
+        q3 = pd.read_csv('./data/reviews_q3.csv')
+        q4 = pd.read_csv('./data/reviews_q4.csv')
+        q2['date'] = pd.to_datetime(q2['date']).dt.date
+        q3['date'] = pd.to_datetime(q3['date']).dt.date
+        q4['date'] = pd.to_datetime(q4['date']).dt.date
+
+        # Drop Duplicates
+        reviews = pd.concat([q1,q2,q3,q4])
+        reviews = reviews.drop_duplicates()
+
+        reviews.to_csv('./src/extracted/reviews.csv', encoding='utf-8', index=False, header=True)
+
         self.completed = True
 
 
@@ -141,6 +175,18 @@ class Users(luigi.Task):
         return self.completed
 
     def run(self):
+        xls = pd.ExcelFile('./data/file_1000.xls')
+        user1 = pd.read_excel(xls, 'Sheet1')
+        user2 = pd.read_excel(xls, 'Sheet2')
+
+        # Adjust Columns
+        user1 = user1.drop(['Unnamed: 0', 'First Name.1'], axis=1)
+        user2 = user2.drop('Unnamed: 0', axis=1)
+
+        # Drop Duplicates
+        user = pd.concat([user1, user2]).drop_duplicates()
+
+        user.to_csv('./src/extracted/user.csv', encoding='utf-8', index=False, header=True)
 
         self.completed = True
 
@@ -152,6 +198,27 @@ class Tweet(luigi.Task):
     def requires(self):
 
         return []
+
+    def complete(self):
+
+        return self.completed
+
+    def run(self):
+
+        tweet = pd.read_json('./data/tweet_data.json', lines=True)
+        tweet = tweet.dropna(axis=1, how='all')
+        tweet.to_csv('./src/extracted/tweet.csv', encoding='utf-8', index=False, header=True)
+
+        self.completed = True
+
+
+class ExtractCompleted(luigi.Task):
+
+    completed = False
+
+    def requires(self):
+
+        return [Chinook(), Database(), Disaster(), Users(), Tweet(), Reviews()]
 
     def complete(self):
 
